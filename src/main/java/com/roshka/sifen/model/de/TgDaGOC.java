67f0f7828873eb80d1ea7f76f8bf0debe4fa3388
Pablo Santa Cruz <pablo@roshka.com.py>
2020-05-23 21:18:19 -0400

    Siguen los tipos yendo.

--- src/main/java/com/roshka/sifen/model/de/TgDaGOC.java
+++ src/main/java/com/roshka/sifen/model/de/TgDaGOC.java
@@ -7,9 +7,38 @@ public class TgDaGOC {
     private LocalDateTime dFeEmiDE;
     private TGOpeCom gOpeCom;
     private TgEmis gEmis;
+    private TgDatRec gDatRec;
 
+    public LocalDateTime getdFeEmiDE() {
+        return dFeEmiDE;
+    }
 
+    public void setdFeEmiDE(LocalDateTime dFeEmiDE) {
+        this.dFeEmiDE = dFeEmiDE;
+    }
 
+    public TGOpeCom getgOpeCom() {
+        return gOpeCom;
+    }
 
+    public void setgOpeCom(TGOpeCom gOpeCom) {
+        this.gOpeCom = gOpeCom;
+    }
+
+    public TgEmis getgEmis() {
+        return gEmis;
+    }
+
+    public void setgEmis(TgEmis gEmis) {
+        this.gEmis = gEmis;
+    }
+
+    public TgDatRec getgDatRec() {
+        return gDatRec;
+    }
+
+    public void setgDatRec(TgDatRec gDatRec) {
+        this.gDatRec = gDatRec;
+    }
 
 }
