5ac53796cdaa947344d12a8463d380d03739f3a1
Diego Schulz <dschulz@gmail.com>
2022-07-27 08:51:11 -0400

    Agregar descriptor de modulo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+ 
+module RshkJsifenlib {
+
+    requires java.base;
+    requires java.logging;
+    requires jakarta.xml.ws;
+    requires jakarta.xml.soap;
+    requires java.xml.crypto;
+    requires java.xml.soap;
+    requires java.xml;
+    requires jakarta.activation;
+    
+    exports com.roshka.sifen;
+    exports com.roshka.sifen.core;
+}
