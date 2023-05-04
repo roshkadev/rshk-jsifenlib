e89abbccefaf54232693c2ccf68e48b1dcf54a9d
David Ayala <david.eaj@gmail.com>
2023-05-04 10:08:49 -0400

    Se agrega nuevo campo segun NT 13 de SIFEN

--- src/main/java/com/roshka/sifen/core/fields/request/de/TgCamIVA.java
+++ src/main/java/com/roshka/sifen/core/fields/request/de/TgCamIVA.java
@@ -18,6 +18,7 @@ public class TgCamIVA extends SifenObjectBase {
     private BigDecimal dTasaIVA;
     private BigDecimal dBasGravIVA;
     private BigDecimal dLiqIVAItem;
+    private BigDecimal dBasExe;
 
     public void setupSOAPElements(SOAPElement gCamItem, CMondT cMoneOpe, BigDecimal dTotOpeItem) throws SOAPException {
         SOAPElement gCamIVA = gCamItem.addChildElement("gCamIVA");
@@ -25,30 +26,33 @@ public class TgCamIVA extends SifenObjectBase {
         gCamIVA.addChildElement("dDesAfecIVA").setTextContent(this.iAfecIVA.getDescripcion());
         gCamIVA.addChildElement("dPropIVA").setTextContent(String.valueOf(this.dPropIVA));
         gCamIVA.addChildElement("dTasaIVA").setTextContent(String.valueOf(this.dTasaIVA));
-        //am2811
         int scale = cMoneOpe.name().equals("PYG") ? 0 : 2;
-        dTotOpeItem = dTotOpeItem.setScale(scale, RoundingMode.HALF_UP);
+        BigDecimal hundred = BigDecimal.valueOf(100);
+        BigDecimal propIVA = this.dPropIVA.divide(hundred, 2, RoundingMode.HALF_UP);
         if (this.iAfecIVA.getVal() == 1 || this.iAfecIVA.getVal() == 4) {
-            BigDecimal propIVA = this.dPropIVA.divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
-
             if (this.dTasaIVA.equals(BigDecimal.valueOf(10))) {
                 this.dBasGravIVA = dTotOpeItem.multiply(propIVA).divide(BigDecimal.valueOf(1.1), scale, RoundingMode.HALF_UP);
-                this.dLiqIVAItem = dTotOpeItem.multiply(propIVA).divide(BigDecimal.valueOf(11), scale, RoundingMode.HALF_UP);
             } else if (this.dTasaIVA.equals(BigDecimal.valueOf(5))) {
                 this.dBasGravIVA = dTotOpeItem.multiply(propIVA).divide(BigDecimal.valueOf(1.05), scale, RoundingMode.HALF_UP);
-                this.dLiqIVAItem = dTotOpeItem.multiply(propIVA).divide(BigDecimal.valueOf(21), scale, RoundingMode.HALF_UP);
             }
 
-            // this.dLiqIVAItem = dTotOpeItem.subtract(this.dBasGravIVA);
-            // am22_11
-            // this.dLiqIVAItem = this.dLiqIVAItem.setScale(2, RoundingMode.HALF_UP);
+            this.dLiqIVAItem = dTotOpeItem.subtract(this.dBasGravIVA);
         } else {
             this.dBasGravIVA = BigDecimal.ZERO;
             this.dLiqIVAItem = BigDecimal.ZERO;
         }
 
+        if (this.iAfecIVA.getVal() == 4) {
+            // Actualización: https://ekuatia.set.gov.py/portal/ekuatia/detail?content-id=/repository/collaboration/sites/ekuatia/documents/documentacion/documentacion-tecnica/NT_E_KUATIA_013_MT_V150.pdf
+            // E737 = [100 * EA008 * (100 – E733)] / [10000 + (E734 * E733)]
+            this.dBasExe = (dTotOpeItem.multiply(hundred.subtract(propIVA)).multiply(hundred)).divide((this.dTasaIVA.multiply(dPropIVA)).add(BigDecimal.valueOf(10000)), scale, RoundingMode.HALF_UP);
+        } else {
+            this.dBasExe = BigDecimal.valueOf(0);
+        }
+
         gCamIVA.addChildElement("dBasGravIVA").setTextContent(String.valueOf(this.dBasGravIVA));
         gCamIVA.addChildElement("dLiqIVAItem").setTextContent(String.valueOf(this.dLiqIVAItem));
+        gCamIVA.addChildElement("dBasExe").setTextContent(String.valueOf(this.dBasExe));
     }
 
     @Override
@@ -68,6 +72,9 @@ public class TgCamIVA extends SifenObjectBase {
                 break;
             case "dLiqIVAItem":
                 this.dLiqIVAItem = new BigDecimal(ResponseUtil.getTextValue(value));
+                break;
+            case "dBasExe":
+                this.dBasExe = new BigDecimal(ResponseUtil.getTextValue(value));
                 break;
         }
     }
@@ -100,7 +107,24 @@ public class TgCamIVA extends SifenObjectBase {
         return dBasGravIVA;
     }
 
+    public void setdBasGravIVA(BigDecimal dBasGravIVA) {
+        this.dBasGravIVA = dBasGravIVA;
+    }
+
     public BigDecimal getdLiqIVAItem() {
         return dLiqIVAItem;
     }
+
+    public void setdLiqIVAItem(BigDecimal dLiqIVAItem) {
+        this.dLiqIVAItem = dLiqIVAItem;
+    }
+
+    public BigDecimal getdBasExe() {
+        return dBasExe;
+    }
+
+    public void setdBasExe(BigDecimal dBasExe) {
+        this.dBasExe = dBasExe;
+    }
+
 }
